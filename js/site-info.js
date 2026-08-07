6ba79c5ea18a8558a55078d0d2b96dc8bcc2968d
Jeremy <jexjackson98@gmail.com>
2026-08-07 11:18:30 -0400

    Capitalization fixes that blocked the writing stuff from applying

--- js/site-info.js
+++ js/site-info.js
@@ -1,5 +1,5 @@
 const siteInfo = {
     name: "Jeremy Jackson",
-    about: "Hey there! I'm a man of many hats: writing, coding, building computers, writing music, stock trading, accounting, and anything else I feel inclined to try. The backbone of all this is a mindset: 'a structured approach can be applied to anything, and procrastination is a boring, fruitless choice.' I've never missed a deadline and likely never will. I hope I can help you with whatever we're working on next!",
-    image: "images/profile444.png"
+    about: "Hey there! I'm a man of many hats: writing, coding, building computers, writing music, stock trading, accounting, and anything else I feel inclined to try. This portfolio is in itself, an example of that. I had no idea how to make a live site before I decided this would be a beneficial thing to have! The backbone of all this is a mindset: 'a structured approach can be applied to anything, and procrastination is a boring, fruitless choice.' I've never missed a deadline and likely never will. I hope I can help you with whatever we're working on next!",
+    image: "images/Profile444.jpg"
 };
